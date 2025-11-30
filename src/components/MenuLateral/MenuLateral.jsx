21cdd8b3105031e834f72e15f3ad8f5d2aed961a
Nehemias <nemifalkon@gmail.com>
2025-11-30 02:33:25 -0500

    frontendconproductosycategorias

--- src/components/MenuLateral/MenuLateral.jsx
+++ src/components/MenuLateral/MenuLateral.jsx
@@ -2,14 +2,26 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './MenuLateral.css'
 import categoriasApi from '../../api/categoriasApi'
+import { useState, useEffect } from 'react'
 
 const MenuLateral = ({ activeCategory }) => {
-    const categorias = categoriasApi.get()
+
+    const [todasLasCategorias, setTodasLasCategorias] = useState([])
+
+    const handleOnLoad = async () => {
+        const rawcategorias = await categoriasApi.findAll()
+        setTodasLasCategorias(rawcategorias)
+    }
+
+    useEffect(()=>{
+        handleOnLoad()
+    }, [])
+
     return (
         <nav className="menu-lateral">
             <h3 className="menu-titulo">Categorías</h3>
             <ul className="menu-lista">
-                {categorias.map(cat => (
+                {todasLasCategorias.map(cat => (
                     <li key={cat.nombre} className="menu-item">
                         <Link 
                             to={`/catalogo/${cat.ruta}`}
